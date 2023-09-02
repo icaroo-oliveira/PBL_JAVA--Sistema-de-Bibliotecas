c43e3ce77db8d93e4b006f8a2959f53b721c7da7
icaroo-oliveira <icarooo10.j@gmail.com>
2023-09-01 22:40:44 -0300

    codigo_pbl1
    começando

---
+++ pbl_1/Operadores.java
@@ -0,0 +1,19 @@
+package pbl_1;
+
+public class Operadores extends Pessoa{
+    private String cargo;
+    private String senha_de_acesso;
+
+    public void Registrar_livros(){
+
+    }
+
+    public void Pesquisar_livros(){
+
+    }
+
+    public void Solicitar_emprestimos(){
+
+    }
+
+}
---
+++ pbl_1/Pessoa.java
@@ -0,0 +1,9 @@
+package pbl_1;
+
+public class Pessoa {
+    private String nome;
+    private String endereco;
+    private String num_identifica;
+    private String telefone;
+
+}
