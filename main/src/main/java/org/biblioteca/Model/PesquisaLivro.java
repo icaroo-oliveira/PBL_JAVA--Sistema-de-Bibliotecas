464dd6a88c5bf2a144d0615596166bc74b58e67e
icaroo-oliveira <icarooo10.j@gmail.com>
2023-10-02 19:22:53 -0300

    Alteração na classe PesquisaLivro
    1-listas não sao mais atributos, ficam somente dentro dos métodos
    2- adcionados comentarios java doc

--- main/src/main/java/org/biblioteca/Model/PesquisaLivro.java
+++ main/src/main/java/org/biblioteca/Model/PesquisaLivro.java
@@ -7,72 +7,94 @@ import java.util.Collections;
 import java.util.List;
 import java.util.*;
 
+/**
+ * Classe responsável pela pesquisa de livros, por autor,categoria... também retorna o top livro
+ * de uma pessoa
+ */
 public class PesquisaLivro {
 
-    final private List<Livro> lista_autor = new ArrayList<>();
-    final private List<Livro> lista_categoria = new ArrayList<>();
-    final private List<Livro> lista_titulo = new ArrayList<>();
 
-    final private List<Livro> lista_ISBN = new ArrayList<>();
-
-
+    /**
+     * Pesquisa de livros pelo autor
+     * @param autor nome do autor do livro
+     * @return uma lista com todos os livros desse autor
+     */
     public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> lista_autor = new ArrayList<>();
 
         for(Livro livro:DAO.getLivroDAO().findMany()){
             if(livro.getAutor().equalsIgnoreCase(autor)) {
-                this.lista_autor.add(livro);
+                lista_autor.add(livro);
             }
         }
 
-        if (!this.lista_autor.isEmpty()){
-            return this.lista_autor;
+        if (!lista_autor.isEmpty()){
+            return lista_autor;
         }else{
             return Collections.emptyList();
         }
     }
 
+    /**
+     * Pesquisa de livros por categoria
+     * @param categoria o tipo de categoria do livro
+     * @return uma lista de livros dessa determinada categoria
+     */
     public List<Livro> pesquisarPorCategoria(String categoria) {
+        List<Livro> lista_categoria = new ArrayList<>();
         for(Livro livro:DAO.getLivroDAO().findMany()){
             if(livro.getCategoria().equalsIgnoreCase(categoria)) {
-                this.lista_categoria.add(livro);
+                lista_categoria.add(livro);
             }
         }
 
-        if (!this.lista_autor.isEmpty()){
-            return this.lista_categoria;
+        if (!lista_categoria.isEmpty()){
+            return lista_categoria;
         }else{
             return Collections.emptyList();
         }
         // Lógica de pesquisa por categoria.
     }
+
+    /**
+     * Pesquia de livros por titulo
+     * @param titulo o nome do livro(s) pesquisado
+     * @return uma lista de todos os livros com esse titulo
+     */
     public List<Livro> pesquisarPorTitulo(String titulo) {
+        List<Livro> lista_titulo = new ArrayList<>();
         for(Livro livro:DAO.getLivroDAO().findMany()){
             if(livro.getTitulo().equalsIgnoreCase(titulo)) {
-                this.lista_titulo.add(livro);
+                lista_titulo.add(livro);
             }
         }
 
-        if (!this.lista_autor.isEmpty()){
-            return this.lista_titulo;
+        if (!lista_titulo.isEmpty()){
+            return lista_titulo;
         }else{
             return null;
         }
 
-
-
         // Lógica de pesquisa por título.
     }
 
+    /**
+     * Uma pesquisa pelo numero de ISBN dos livros
+     * @param ISBN isbn é uma identidade do livro
+     * @return retorna todos os livros com esse mesmo ISBN
+     */
     public List<Livro> pesquisarPorISBN(int ISBN) {
+        List<Livro> lista_ISBN = new ArrayList<>();
+
 
         for(Livro livro:DAO.getLivroDAO().findMany()){
             if(livro.getISBN()==ISBN) {
-                this.lista_ISBN.add(livro);
+                lista_ISBN.add(livro);
             }
         }
 
-        if (!this.lista_autor.isEmpty()){
-            return this.lista_ISBN;
+        if (!lista_ISBN.isEmpty()){
+            return lista_ISBN;
         }else{
             return Collections.emptyList();
         }
@@ -80,7 +102,10 @@ public class PesquisaLivro {
         // Lógica de pesquisa por título.
     }
 
-
+    /**
+     * Filtra livros pela popularidades, mais emprestados
+     * @return retorna uma lista com todos os livros, da ordem do mais popular para o menos popular
+     */
     public List<Livro> TopLivros(){
 
         List<Livro> livros = DAO.getLivroDAO().findMany();
