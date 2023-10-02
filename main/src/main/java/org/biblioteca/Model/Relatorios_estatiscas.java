24013b784d0a44e630c06e48467adcf4408476c9
icaroo-oliveira <icarooo10.j@gmail.com>
2023-10-02 19:21:08 -0300

    Alteração na Classe Relatorios_estatisticas, colocando listas dentro dos métodos
    1-movendo as listas de atributos da classe para ficar somente dentro dos métodos
    2-adicionados comentários java doc
    3-modificação na lógica para descobrir livros atrasados

--- main/src/main/java/org/biblioteca/Model/Relatorios_estatiscas.java
+++ main/src/main/java/org/biblioteca/Model/Relatorios_estatiscas.java
@@ -8,63 +8,96 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Classe responsável por estatitiscas relacionados aos livros, emprestados, atrasados, reservados
+ * também consegue uma lista com os empréstimos/ livros daquele usuário
+ */
 public class Relatorios_estatiscas {
 
-    final private List<Livro> lista_emprestados = new ArrayList<>();
-    final private List<Livro> lista_atrasados = new ArrayList<>();
-    final private List<Livro> lista_reservados = new ArrayList<>();
 
-
+    /**
+     * método responsável por conseguir somente os livros que estão emprestados
+     * @return retorna uma lista com livros que estão emprestados
+     */
     public List<Livro> getLista_emprestados() {
+        List<Livro> lista_emprestados = new ArrayList<>();
 
         for(Livro livro: DAO.getLivroDAO().findMany()){
             if(!livro.getDisponibilidade()) {
-                this.lista_emprestados.add(livro);
+                lista_emprestados.add(livro);
             }
         }
 
-        if (!this.lista_emprestados.isEmpty()){
-            return this.lista_emprestados;
+        if (!lista_emprestados.isEmpty()){
+            return lista_emprestados;
         }else{
             return Collections.emptyList();
         }
     }
 
 
-    public List<Livro> getLista_atrasados() {
+    /**
+     * Método responsável por conseguir os livros atrasdos
+     * @param parametro passa uma data para servir como base no cálculo dos livros atrasados
+     * @return retorna uma lista com livros atrasados
+     */
+    public List<Livro> getLista_atrasados(LocalDate parametro) {
+        List<Livro> lista_atrasados = new ArrayList<>();
 
+        //System.out.println(DAO.getLivroDAO().findMany().size());
 
         for(Livro livro: DAO.getLivroDAO().findMany()){
-            if(!livro.getDisponibilidade() && (int) ChronoUnit.DAYS.between(livro.getEmprestimo().getData_devolucao_esperada(), LocalDate.now())<0) {
-                this.lista_atrasados.add(livro);
+
+            System.out.println(livro.getEmprestimo());
+            if(livro.getEmprestimo()!=null){
+                int unidade = (int) ChronoUnit.DAYS.between(livro.getEmprestimo().getData_emprestimo(), parametro);
+
+                if(!livro.getDisponibilidade() && unidade>7) {
+                    lista_atrasados.add(livro);
+                }
             }
+
+
         }
 
-        if (!this.lista_atrasados.isEmpty()){
-            return this.lista_atrasados;
+        if (!lista_atrasados.isEmpty()){
+            return lista_atrasados;
         }else{
             return Collections.emptyList();
         }
     }
 
+    /**
+     * Métodos responsável por conseguir somente os livros reservados
+     * @return retorna uma lista com todos os livros que foram reservados
+     */
     public List<Livro> getLista_reservados() {
+        List<Livro> lista_reservados = new ArrayList<>();
 
 
         for(Livro livro: DAO.getLivroDAO().findMany()){
             if(!livro.getFila().isEmpty()) {
-                this.lista_reservados.add(livro);
+                lista_reservados.add(livro);
             }
         }
 
-        if (!this.lista_reservados.isEmpty()){
-            return this.lista_reservados;
+        if (!lista_reservados.isEmpty()){
+            return lista_reservados;
         }else{
             return Collections.emptyList();
         }
     }
 
-
+    /**
+     * Método responsável por pegar todos os empréstimos ja feito por uma pessoa
+     * @param id o id do usuário que irá pegar o historico
+     * @return retorna uma lista com todos emprestimos feito por aquele usuário
+     * @throws Exception lança uma exceção se não encontrar o usuário
+     */
     public List<Emprestimo> getUsuarioEmprestimos(int id) throws Exception {
+
+
+        System.out.println(id);
 
         if(!DAO.getUsuarioDAO().findById(id).getHistorico_livro().isEmpty()){
             return DAO.getUsuarioDAO().findById(id).getHistorico_livro();
