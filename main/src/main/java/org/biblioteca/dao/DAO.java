9248ec89761714c67006847abf9e656537f781dd
icaroo-oliveira <icarooo10.j@gmail.com>
2023-10-06 09:01:18 -0300

    Adicionado java doc a interface CRUD e a classe DAO, também java doc nas exceções de usuário e livro
    Adicionado java doc a interface CRUD e a classe DAO, também java doc nas exceções de usuário e livro (métodos e atributos em todos)

--- main/src/main/java/org/biblioteca/dao/DAO.java
+++ main/src/main/java/org/biblioteca/dao/DAO.java
@@ -13,20 +13,50 @@ import org.biblioteca.dao.Usuario.UsuarioDAOLista;
 
 /**
  * Classe DAO, aplicando padrão singleton para todos os DAO
+ * é usada acessando os atributos estaticos da classe
  */
 public class DAO {
 
+    /**
+     * atributo da CLASSE DAO
+     * atributo estatico do tipo EmprestimoDAO
+     * usado para manter uma unica instancia do DAO dos emprestimos
+     */
     private static EmprestimoDAO EmprestimoDAO;
+    /**
+     * atributo da CLASSE DAO
+     * atributo estatico do tipo LivroDAO
+     * usado para manter uma unica instancia do DAO dos livros
+     */
     private static LivroDAO LivroDAO;
+    /**
+     * atributo da CLASSE DAO
+     * atributo estatito do tipo OperadoresDAO
+     * usado para manter uma unica instancia do DAO dos Operadores
+     */
     private static OperadoresDAO OperadoresDAO;
+    /**
+     * atributo da CLASSE DAO
+     * atributo estatico do tipo UsuarioDAO
+     * usado para manter uma unica instancia do DAO dos usuarios
+     */
     private static UsuarioDAO UsuarioDAO;
 
+    /**
+     * método estatico para aplicar o padrão singleton na classe EmprestimoDAOLista
+     * @return retorna o DAO relacionado aos empréstimos
+     */
     public static EmprestimoDAO getEmprestimoDAO() {
         if (EmprestimoDAO == null) {
             EmprestimoDAO = new EmprestimoDAOLista();
         }
         return EmprestimoDAO;
     }
+
+    /**
+     * método estatico para aplicar o padrão singleton na classe OperadoresDAOLista
+     * @return retorna o DAO relacionado aos operadores
+     */
     public static OperadoresDAO getOperadoresDAO() {
         if (OperadoresDAO == null) {
             OperadoresDAO = new OperadoresDAOLista();
@@ -34,6 +64,10 @@ public class DAO {
         return OperadoresDAO;
     }
 
+    /**
+     * método estatico para aplicar o padrão singleton na classe LivroDAOLista
+     * @return retorna o DAO relacionado aos livros
+     */
     public static LivroDAO getLivroDAO() {
         if (LivroDAO == null) {
             LivroDAO = new LivroDAOLista();
@@ -41,6 +75,10 @@ public class DAO {
         return LivroDAO;
     }
 
+    /**
+     * método estatico para aplicar o padrão singleton na classe UsuarioDAOLista
+     * @return retorna o DAO relacionado aos usuario
+     */
     public static UsuarioDAO getUsuarioDAO() {
         if (UsuarioDAO == null) {
             UsuarioDAO = new UsuarioDAOLista();
--- main/src/main/java/org/biblioteca/excepctions/LivroException.java
+++ main/src/main/java/org/biblioteca/excepctions/LivroException.java
@@ -7,23 +7,56 @@ import org.biblioteca.Model.Livro;
  */
 public class LivroException extends Exception{
 
+    /**
+     * Mensagem estatica relacionada a falha na criação de um livro, na listaDAO ou no momento criação do livro
+     */
     public static final String CREATE = "Não foi possível criar um livro";
 
+    /**
+     * Mensagem estatica relacionada a falha reserva
+     */
     public static final String RESERVA = "Não foi possível reservar o livro";
 
+    /**
+     * mensagem relacionada a falha no delete de um livro na listaDAO
+     */
     public static final String DELETE = "Não foi possível deletar o livro";
 
+    /**
+     * mensagem estatica relacionada a falha na procura por um livro, na listaDAO
+     */
     public static final String FIND ="Não foi possível encontrar esse livro";
 
+    /**
+     * Mensagem relacioanda a falha na atualizao de um livro, listaDAO
+     */
     public static final String UPDATE = "Operação de ATUALIZAÇÃO não realizada.";
 
+    /**
+     * objeto livro que sofrerá o ''CRUD falho''
+     */
     private Livro livro;
+
+    /**
+     * id do objeto que terá falha no CRUD
+     */
     private int id;
+
+    /**
+     * construtor para a classe de exceção
+     * @param message mensagem a ser passada no momento da exceção
+     * @param livro objeto que ocorreu a exceção
+     */
     public LivroException(String message, Livro livro) {
         super(message);
         this.livro = livro;
     }
 
+    /**
+     * construtor para a classe de exceção
+     * @param message mensagem a ser passada no momento da exceção
+     * @param id id do objeto que ocrreu a exceção
+     */
     public LivroException(String message, int id) {
         super(message);
         this.id = id;
