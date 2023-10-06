19cfabecf89cee8457bdfc899ca75d1dc4ecf256
icaroo-oliveira <icarooo10.j@gmail.com>
2023-10-06 08:46:39 -0300

    Implementação das funcionalidades das classes Pesquisalivro e relatoriso_estatisticas nos DAOS livro e usuario/comentários java doc nos métodos e atributos da classe e java doc da interface
    Primeiro foi feita a assinatura dos métodos na interface LivroDAO, onde os seguintes métodos foram colocados:
    1-Pesquisa por autor
    2-Pesquisa por titulo
    3-Pesquisa por ISBN
    4-Pesquisa por categoria
    5-Conseguir a lista de livros mais populares
    6-Conseguir a lista de livros emprestados
    7-Lista dos livros atrasados
    8- Lista dos livros reservados
    
    Foram implementadas esses métodos na classe LivroDAOLista, foi pego os códigos das classes Pesquisa e livro e relatorios e estatitiscas e colocado na classe do livro DAO, basicamente

--- main/src/main/java/org/biblioteca/dao/Livro/LivroDAO.java
+++ main/src/main/java/org/biblioteca/dao/Livro/LivroDAO.java
@@ -1,7 +1,55 @@
 package org.biblioteca.dao.Livro;
 
+import org.biblioteca.Model.Emprestimo;
 import org.biblioteca.dao.CRUD;
 import org.biblioteca.Model.Livro;
 
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * interface pra o LivroDAOLista ela herda da interface CRUD um tipo de classe generica e a exceção
+ */
 public interface LivroDAO extends CRUD<Livro,Exception> {
+
+    /**
+     * assinatura para o método de pesquisa por autor
+     * @param autor autor que será usado para pesquisa
+     * @return lista com todos os livros daquele autor
+     */
+    public List<Livro> pesquisarPorAutor(String autor);
+
+    /**
+     * assinatura para o método de pesquisa por categoria
+     * @param categoria categoria que sera usada para pesquisa
+     * @return lista com todos os livros daquela categoria
+     */
+    public List<Livro> pesquisarPorCategoria(String categoria);
+
+    /**
+     * assinatura para o método de pesquisa usanado a o titulo do livro
+     * @param titulo titulo do livro
+     * @return retorna uma lista composta por livros daquele mesmo titulo
+     */
+    public List<Livro> pesquisarPorTitulo(String titulo);
+
+    /**
+     * assinatura para o métodod de pesquisa por isbn
+     * @param ISBN isbn dos livros
+     * @return retorna uma lista de livros com aquele isbn
+     */
+    public List<Livro> pesquisarPorISBN(int ISBN);
+
+    /**
+     * assinatura para o método de geração de top livros
+     * @return retorna uma lista de livros em ordem de popularidade, do mais popular pro menos popular
+     */
+    public List<Livro> TopLivros();
+
+    ///////>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<
+
+    public List<Livro> getLista_emprestados();
+    public List<Livro> getLista_atrasados(LocalDate parametro);
+    public List<Livro> getLista_reservados();
+
 }
--- main/src/main/java/org/biblioteca/dao/Livro/LivroDAOLista.java
+++ main/src/main/java/org/biblioteca/dao/Livro/LivroDAOLista.java
@@ -1,25 +1,43 @@
 package org.biblioteca.dao.Livro;
 
 import org.biblioteca.Model.Livro;
+import org.biblioteca.dao.DAO;
 import org.biblioteca.excepctions.LivroException;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
- * LASSE dao para os livros, servirá para separar a lógica de negocio do armazenamento de dados
+ * CLASSE dao para os livros, servirá para separar a lógica de negocio do armazenamento de dados
  */
 public class LivroDAOLista implements LivroDAO{
 
+    /**
+     *Lista de livros, vai guardar todos os livros ja adicionados ao sistema, em uma lista
+     */
     private List<Livro> lista;
 
+    /**
+     * variável que guardará o próximo ID a ser utilizado, tipo um contador.
+     */
     private int proximoID;
 
+    /**
+     *Construtor para classe, quando iniciado o singleton, é criado uma nova lista e zerado o id para contagem
+     */
     public LivroDAOLista() {
         this.lista = new ArrayList<>();
         this.proximoID = 0;
     }
 
+    /**
+     * método para contagem dos id's
+     * @return retorna o próximo id a ser usado
+     */
     private int getProximoID(){
         return this.proximoID++;
     }
@@ -32,7 +50,7 @@ public class LivroDAOLista implements LivroDAO{
     @Override
     public Livro create(Livro obj) {
         obj.setId(this.getProximoID());
-        lista.add(obj);
+        this.lista.add(obj);
         return obj;
     }
 
@@ -100,7 +118,190 @@ public class LivroDAOLista implements LivroDAO{
             }
         }
         throw new LivroException(LivroException.FIND,id);
-
-
     }
+
+    /**
+     * Pesquisa de livros pelo autor
+     * @param autor nome do autor do livro
+     * @return uma lista com todos os livros desse autor
+     */
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> lista_autor = new ArrayList<>();
+
+        for(Livro livro: this.lista){
+            if(livro.getAutor().equalsIgnoreCase(autor)) {
+                lista_autor.add(livro);
+            }
+        }
+
+        if (!lista_autor.isEmpty()){
+            return lista_autor;
+        }else{
+            return Collections.emptyList();
+        }
+    }
+
+    /**
+     * Pesquisa de livros por categoria
+     * @param categoria o tipo de categoria do livro
+     * @return uma lista de livros dessa determinada categoria
+     */
+    public List<Livro> pesquisarPorCategoria(String categoria) {
+        List<Livro> lista_categoria = new ArrayList<>();
+        for(Livro livro:this.lista){
+            if(livro.getCategoria().equalsIgnoreCase(categoria)) {
+                lista_categoria.add(livro);
+            }
+        }
+
+        if (!lista_categoria.isEmpty()){
+            return lista_categoria;
+        }else{
+            return Collections.emptyList();
+        }
+        // Lógica de pesquisa por categoria.
+    }
+
+
+    /**
+     * Pesquia de livros por titulo
+     * @param titulo o nome do livro(s) pesquisado
+     * @return uma lista de todos os livros com esse titulo
+     */
+    public List<Livro> pesquisarPorTitulo(String titulo) {
+        List<Livro> lista_titulo = new ArrayList<>();
+        for(Livro livro:this.lista){
+            if(livro.getTitulo().equalsIgnoreCase(titulo)) {
+                lista_titulo.add(livro);
+            }
+        }
+
+        if (!lista_titulo.isEmpty()){
+            return lista_titulo;
+        }else{
+            return Collections.emptyList();
+        }
+
+        // Lógica de pesquisa por título.
+    }
+
+
+    /**
+     * Uma pesquisa pelo numero de ISBN dos livros
+     * @param ISBN isbn é uma identidade do livro
+     * @return retorna todos os livros com esse mesmo ISBN
+     */
+    public List<Livro> pesquisarPorISBN(int ISBN) {
+        List<Livro> lista_ISBN = new ArrayList<>();
+
+
+        for(Livro livro:this.lista){
+            if(livro.getISBN()==ISBN) {
+                lista_ISBN.add(livro);
+            }
+        }
+
+        if (!lista_ISBN.isEmpty()){
+            return lista_ISBN;
+        }else{
+            return Collections.emptyList();
+        }
+
+        // Lógica de pesquisa por título.
+    }
+
+
+    /**
+     * Filtra livros pela popularidades, mais emprestados
+     * @return retorna uma lista com todos os livros, da ordem do mais popular para o menos popular
+     */
+    public List<Livro> TopLivros(){
+
+        List<Livro> livros = this.lista;
+
+        if(!livros.isEmpty()){
+            livros.sort(new Comparator<Livro>() {
+                @Override
+                public int compare(Livro l1, Livro l2) {
+                    return Integer.compare(l2.getPopularity(), l1.getPopularity());
+                }
+            });
+            return livros;
+        }else{
+            return Collections.emptyList();
+        }
+    }
+
+
+    /**
+     * método responsável por conseguir somente os livros que estão emprestados
+     * @return retorna uma lista com livros que estão emprestados
+     */
+    public List<Livro> getLista_emprestados() {
+        List<Livro> lista_emprestados = new ArrayList<>();
+
+        for(Livro livro: this.lista){
+            if(!livro.getDisponibilidade()) {
+                lista_emprestados.add(livro);
+            }
+        }
+
+        if (!lista_emprestados.isEmpty()){
+            return lista_emprestados;
+        }else{
+            return Collections.emptyList();
+        }
+    }
+
+
+    /**
+     * Método responsável por conseguir os livros atrasdos
+     * @param parametro passa uma data para servir como base no cálculo dos livros atrasados
+     * @return retorna uma lista com livros atrasados
+     */
+    public List<Livro> getLista_atrasados(LocalDate parametro) {
+        List<Livro> lista_atrasados = new ArrayList<>();
+
+        for(Livro livro: this.lista){
+
+            if(livro.getEmprestimo()!=null){
+                int unidade = (int) ChronoUnit.DAYS.between(livro.getEmprestimo().getData_emprestimo(), parametro);
+
+                if(!livro.getDisponibilidade() && unidade>7) {
+                    lista_atrasados.add(livro);
+                }
+            }
+
+
+        }
+
+        if (!lista_atrasados.isEmpty()){
+            return lista_atrasados;
+        }else{
+            return Collections.emptyList();
+        }
+    }
+
+
+    /**
+     * Métodos responsável por conseguir somente os livros reservados
+     * @return retorna uma lista com todos os livros que foram reservados
+     */
+    public List<Livro> getLista_reservados() {
+        List<Livro> lista_reservados = new ArrayList<>();
+
+
+        for(Livro livro: this.lista){
+            if(!livro.getFila().isEmpty()) {
+                lista_reservados.add(livro);
+            }
+        }
+
+        if (!lista_reservados.isEmpty()){
+            return lista_reservados;
+        }else{
+            return Collections.emptyList();
+        }
+    }
+
 }
