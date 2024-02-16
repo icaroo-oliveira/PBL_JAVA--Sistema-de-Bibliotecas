67e9ed42dfd59dd4c0be34efb97a2905f0f2e4fe
icaroo-oliveira <icarooo10.j@gmail.com>
2024-02-16 20:09:42 -0300

    Modificando HelloAplication, dando commit no module.info
    Modificação do HelloAplication criado automaticamento pelo java fx
    
    Vai abrir uma nova janela para "/com/example/pbljava/hello-view.fxml" que tem como controller a classe HelloController

--- src/main/java/com/example/pbljava/HelloApplication.java
+++ src/main/java/com/example/pbljava/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.pbljava;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,18 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/com/example/pbljava/hello-view.fxml"));
+
+
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+
+        stage.setTitle("Cena 1");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.example.pbljava {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
-    opens com.example.pbljava to javafx.fxml;
+    opens com.example.pbljava to javafx.fxml,javafx.graphics;
     exports com.example.pbljava;
+    exports com.example.pbljava.controller;
+    opens com.example.pbljava.controller to javafx.fxml,javafx.graphics;
+    opens com.example.pbljava.Model;
 }
