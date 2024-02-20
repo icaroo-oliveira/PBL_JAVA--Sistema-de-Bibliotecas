d3d89bfd77bcced31c3414e0e062c5a7d6bb0be7
icaroo-oliveira <icarooo10.j@gmail.com>
2024-02-20 08:33:11 -0300

    Controller e Tela (fxml) para a pagina principal dos operadores
    A página tem um controle de buttons, onde so mostra todas as funcionalidades para o ADM.
    Se o operador for bibliotecario, ele só terá acesso aos recursos de, emprestimo e devolucao, renovar e  crud nos livros
    
    A tela que aparece é a mesma pra ambos, mas como dito os bibliotecarios so teram 3 botoes disponiveis
    
    Quando logado com ADM, os outros 3 botoes sao liberadados. (Controle de operadores,Gerenciamento de acervo,multas,Relatorios,cadastro de usuários...)

---
+++ src/main/java/com/example/pbljava/controller/Operadormain.java
@@ -0,0 +1,148 @@
+package com.example.pbljava.controller;
+
+import com.example.pbljava.Model.Usuario;
+import com.example.pbljava.components.ControladorDados;
+import com.example.pbljava.dao.DAO;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+
+import java.util.Objects;
+
+public class Operadormain {
+
+    @FXML
+    private BorderPane Painelprincipaloperador;
+
+    @FXML
+    private Label nomeoperador;
+
+    @FXML
+    private Button pagina0;
+
+    @FXML
+    private Button pagina1;
+
+    @FXML
+    private Button pagina2;
+
+
+    @FXML
+    private Button pagina3;
+
+    @FXML
+    private Button pagina4;
+
+    @FXML
+    private Button pagina5;
+
+
+    @FXML
+    void initialize() {
+        if(getControlador().getOperador().getCargo().equals("adm")) {
+            pagina3.setVisible(true);
+            pagina4.setVisible(true);
+            pagina5.setVisible(true);
+        } else {
+            pagina3.setVisible(false);
+            pagina4.setVisible(false);
+            pagina5.setVisible(false);
+        }
+    }
+
+
+
+    public ControladorDados getControlador(){
+        ControladorDados controla = ControladorDados.getInstancia();
+        return controla;
+    }
+
+
+
+
+
+
+
+
+
+    @FXML
+    void pagina0Action(ActionEvent event) {
+        System.out.println("melancia");
+        //ControladorDados controla = ControladorDados.getInstancia();
+        //controla.setOperadorr(this);
+        getControlador().setOperadorr(this);
+        openPage("/com/example/pbljava/pagina_emp1.fxml");
+    //emprestimo
+    }
+
+    @FXML
+    void pagina1Action(ActionEvent event) {
+        System.out.println("melancia");
+
+        //ControladorDados controla = ControladorDados.getInstancia();
+        //controla.setOperadorr(this);
+
+        getControlador().setOperadorr(this);
+
+        openPage("/com/example/pbljava/EditandoEmp.fxml");//nothing
+
+    }
+
+    @FXML
+    void pagina2Action(ActionEvent event) {
+        System.out.println("melancia");
+        openPage("/com/example/pbljava/page2.fxml");//livros
+
+    }
+
+    @FXML
+    void pagina3Action(ActionEvent event) {
+
+        openPage("/com/example/pbljava/CriarUsuario.fxml");
+
+
+    }
+
+    @FXML
+    void pagina4Action(ActionEvent event) {
+
+        openPage("/com/example/pbljava/ADDOperador.fxml");
+
+
+    }
+
+    @FXML
+    void pagina5Action(ActionEvent event) {
+
+        openPage("/com/example/pbljava/relatorio.fxml");
+
+
+    }
+
+    private void openPage(String url){
+        Parent root = null;
+        try{
+            root= FXMLLoader.load(Objects.requireNonNull(getClass().getResource(url)));
+        }catch (Exception e){
+            //logica
+        }
+
+        this.Painelprincipaloperador.setCenter(root);
+
+    }
+
+    public BorderPane getP(){
+        return this.Painelprincipaloperador;
+    }
+
+
+}
