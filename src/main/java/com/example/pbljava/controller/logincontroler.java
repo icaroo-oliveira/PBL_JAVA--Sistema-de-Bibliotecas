e63e95e3bd74995b07b05337ab1c367c6fac12cd
icaroo-oliveira <icarooo10.j@gmail.com>
2024-03-07 12:09:14 -0300

    Criando campo para senha(oculta a senha)

--- src/main/java/com/example/pbljava/controller/logincontroler.java
+++ src/main/java/com/example/pbljava/controller/logincontroler.java
@@ -27,11 +27,12 @@ public class logincontroler {
     @FXML
     private Label labelerror1;
     @FXML
-    private TextField senharentrar;
+    private PasswordField senharentrar;
 
     @FXML
     void botaoentrarAction(ActionEvent event) throws Exception {
 
+        System.out.println("ok");
         try {
             Operadores operador = DAO.getOperadoresDAO().findById(Integer.parseInt(Identrar.getText()));
             String senha =senharentrar.getText();
@@ -46,39 +47,28 @@ public class logincontroler {
 
 
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/pbljava/operatormain.fxml"));
-                System.out.println("hi");
+                //System.out.println("hi");
 
                 parent = fxmlLoader.load();
 
-                System.out.println("hi");
+                //System.out.println("hi");
                 Scene scene = new Scene(parent);
 
-                System.out.println("sou");
+                //System.out.println("sou");
                 Stage stage = (Stage) botaoentrar.getScene().getWindow();
 
                 //novo
-                stage.setMinWidth(1000);
-                stage.setMaxWidth(1200);
-                stage.setMinHeight(800);
-                stage.setMaxHeight(1000);
+
 
                 stage.setWidth(900);
                 stage.setHeight(700);
 
 
-                stage.centerOnScreen();
-
-                //novo
-
-
-
-
-
-
-
-                stage.setTitle("Cena 1");
+                stage.setTitle("Janela do Operador");
 
                 stage.setScene(scene);
+                stage.sizeToScene();
+                stage.centerOnScreen();
                 stage.show();
 
 
